f8f1c15892411e09a3866a7fc9dc8a3ab88b9837
Tom Parker-Shemilt <palfrey@tevp.net>
2024-05-15 20:51:58 +0100

    Split out RP2040 board config

---
+++ src/boards/mod.rs
@@ -0,0 +1,15 @@
+use crate::address_range::AddressRange;
+
+pub trait BoardConfig {
+    fn flash_sector_erase_size(&self) -> u32;
+    fn main_ram_start(&self) -> u32;
+    fn main_ram_end(&self) -> u32;
+    fn xip_ram_start(&self) -> u32;
+    fn xip_ram_end(&self) -> u32;
+    fn family_id(&self) -> u32;
+
+    fn address_ranges_flash(&self) -> Vec<AddressRange>;
+    fn address_range_ram(&self) -> Vec<AddressRange>;
+}
+
+pub mod rp2040;
